b15d29439f383c045f5bc1b65643697aaf63e9b7
PenguGH <130692446+PenguGH@users.noreply.github.com>
2024-04-28 18:24:34 -0700

    implemented TaskFactory.java

--- src/main/java/TaskFactory.java
+++ src/main/java/TaskFactory.java
@@ -1,2 +1,32 @@
 public class TaskFactory {
+    private static TaskFactory instance;
+    int date;
+
+    // constructor
+    private TaskFactory(){
+
+    }
+
+    // Gets a singleton instance of the TaskFactory
+    public static TaskFactory getInstance() {
+        if (instance == null) {
+            instance = new TaskFactory();
+        }
+        return instance;
+    }
+
+    // To specify which task type to create
+    public Task createTask(String type) {
+        switch (type) {
+            case "RecurringTask":
+                return new RecurringTask();
+            case "TransientTask":
+                return new TransientTask();
+            case "AntiTask":
+                return new AntiTask();
+            default:
+                return null;
+        }
+    }
 }
+
