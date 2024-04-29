bb1da01a35f4ab63fe7416cc8e5caab871de1fb0
PenguGH <130692446+PenguGH@users.noreply.github.com>
2024-04-28 17:38:45 -0700

    added the Task Interface to Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,26 @@
+import java.util.List;
+
 public interface Task {
+   String getName();
+   void setName(String name);
+
+   String getType();
+   void setType(String type);
+
+   float getStartTime();
+   void setStartTime(float startTime);
+
+   float getDuration();
+   void setDuration(float duration);
+
+   int getStartDate();
+   void setStartDate(int startDate);
+
+   int getEndDate();
+   void setEndDate(int endDate);
+
+   List<Task> getLinkedTasks();
+   void addLinkedTask(Task task);
 }
 
 
