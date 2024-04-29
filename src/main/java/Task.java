7d9f43e14f655d197f6d4756802d00b0d05c2427
PenguGH <130692446+PenguGH@users.noreply.github.com>
2024-04-28 18:07:05 -0700

    implemented TransientTask class in Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -112,7 +112,91 @@ class RecurringTask implements Task {
 }
 
 class TransientTask implements Task {
+    private String name;
+    private String type;
+    private float startTime;
+    private float duration;
+    private int startDate;
 
+    // Contructor
+    public TransientTask() {
+    
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public float getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public void setStartTime(float startTime) {
+        this.startTime = startTime;
+    }
+
+    @Override
+    public float getDuration() {
+        return duration;
+    }
+
+    @Override
+    public void setDuration(float duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public int getStartDate() {
+        return startDate;
+    }
+
+    @Override
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
+    }
+
+    @Override
+    public int getEndDate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEndDate'");
+    }
+
+    @Override
+    public void setEndDate(int endDate) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setEndDate'");
+    }
+
+    @Override
+    public List<Task> getLinkedTasks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getLinkedTasks'");
+    }
+
+    @Override
+    public void addLinkedTask(Task task) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addLinkedTask'");
+    }
+    
 }
 
 class AntiTask implements Task {
